df30ea7a5a7f3c30c617669db279192a04d12721
Gurdeep <Gurdeep@Gurdeeps.local>
2017-01-27 20:05:35 +0530

    Initial Commit

---
+++ HelperFiles/AppMessages.swift
@@ -0,0 +1,15 @@
+//
+//  AppMessages.swift
+//  AppUserDefaults
+//
+//  Created by Gurdeep on 15/12/16.
+//  Copyright © 2016 Gurdeep. All rights reserved.
+//
+
+import Foundation
+
+struct AppMessages {
+
+    static let ErrorMessage = ""
+    static let InternetNotFound = "No Internet Connection Available"
+}
